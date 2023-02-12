bf8ea3e2fb2fe4ba25541146c2302431cfdcefd6
aishwarya <98905717+aishwaryasuradkar@users.noreply.github.com>
2023-02-12 19:41:30 +0530

    changes made successfully

--- src/basics/basics.controller.ts
+++ src/basics/basics.controller.ts
@@ -1,59 +1,109 @@
-import { Body, Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { UserDto } from 'DTO/user';
 import { get } from 'http';
 import { BasicsService } from './basics.service';
 
 @Controller('basics')
 export class BasicsController {
-    constructor (private readonly service:BasicsService){}
-    @Get('getdata')
-    async getdata(){
-        return 'parag is very chutiya boy!<3';
-    }
+  constructor(private readonly service: BasicsService) {}
 
-    @Get ('givedata')
-    async givedata(){
-        return 'adimo is the best mentor!:)';
-    }
-    @Get('gd')
-    async gd(){
-       let a="My name is aishwarya";
-       return a;
-    }
-    @Get('addition')
-    async addition(){
-        let a=1,b=2,c;
-        c=a+b;
-        return c;
-    }
-    @Get('a')
-    async a(@Query()x:string){
-         let temp=x;
-         return temp;
+  // Get requests
 
-    }
-    @Get('ab')
-    async ab(@Query()x:number,@Query()y:number,@Query()z:string){
-       let a:number=x+y;
-       let b=z;
-       return x;
-    }
-    @Get('bc')
-    async bc(@Body()body:any){
-        let tempt=body.name;
-        return tempt;
+  @Get('getdata')
+  async getdata() {
+    return 'parag is very chutiya boy!<3';
+  }
 
+  @Get('givedata')
+  async givedata() {
+    return 'adimo is the best mentor!:)';
+  }
+  @Get('gd')
+  async gd() {
+    let a = 'My name is aishwarya';
+    return a;
+  }
+  @Get('addition')
+  async addition() {
+    let a = 1,
+      b = 2,
+      c;
+    c = a + b;
+    return c;
+  }
+  @Get('a')
+  async a(@Query() x: string) {
+    let temp = x;
+    return temp;
+  }
+  @Get('ab')
+  async ab(@Query() x: number, @Query() y: number, @Query() z: string) {
+    let a: number = x + y;
+    let b = z;
+    return x;
+  }
+  @Get('bc')
+  async bc(@Body() body: any) {
+    let tempt = body.name;
+    return tempt;
+  }
+  @Get('c')
+  async c(@Body() body: any) {
+    let tem = body.id;
+    if (tem == 1111) {
+      console.log('Id accepted');
+    } else {
+      console.log('Id is not accepted');
     }
-    @Get('c')
-    async c(@Body()body:any){
-        let tem=body.id;
-        if(tem==1111){
-           console.log('Id accepted');
-        }
-        else{
-           console.log('Id is not accepted');
-        }
+  }
+  // hw- to create a get req and get id as a para and return it
 
-    }
+@Get('f')
+async f(@Body() body:any){
+  let tem= body.id;
+  return tem;
+}
+
+@Get('g')
+async g(@Query() x:number){
+  let temp=x;
+  return temp
+}
+
+  //Post requests
+
+  @Post()
+  async createUser(@Body() body: any) {
+    const vab = this.service.createUserInDb(body.email, body.pass);
+    return vab;
+  }
+
+//   hw-name,surname 
+
+@Post('d')
+async d(@Body()body:any){
+  const varb = this.service.createUserInDb2(body.namee,body.surnamee);
+  return varb;
+}
+
+@Post('e')
+async e(@Body()body:UserDto){
+  const varib= this.service.userdto(body.otp, body.phnno);
+  return varib;
+
+}
+// hw- to create a get req and get id as a para and return it
+@Post('ef')
+async ef(@Body () body:any){
+  // syntax of typescript/javascript 
+  const v= {
+    'name':"Aditya", 
+    'phnno':1234567890
+    
+  }
+  return v.name;
 
 }
 
+
+}
--- src/basics/basics.service.ts
+++ src/basics/basics.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BasicsService {}
+export class BasicsService {
+  email_1: string = 'adii@pineset.com';
+  pass_1: string = '1234';
+
+  name_1:string='Aditya';
+  surname_1:string='Motale';
+
+  otp_1:number=12345;
+  phnno_1:number=1140;
+
+ async createUserInDb2(namee:string, surnamee:string){
+if(namee==this.name_1 && surnamee==this.surname_1){
+  return 'Access Granted'
+}
+else{
+  return 'access denied!! Calling 911!!';
+}
+}
+
+  async createUserInDb(email: string, passs: string) {
+    if (email == this.email_1 && passs == this.pass_1) {
+      return 'Access granted';
+    } else {
+      return 'parag';
+      // return 'Access denied! calling 911!!';
+    }
+  }
+
+  async userdto(otpp:number, phnnoo:number){
+    if (otpp==this.otp_1 && phnnoo==this.phnno_1){
+      return 'Access granted!!'
+    }
+    else{
+      return 'Access denied!! calling 911!!'
+    }
+
+  }
+ }
