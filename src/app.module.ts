0dae7ba3484793f85f1383c635f687540a059286
aishwarya <98905717+aishwaryasuradkar@users.noreply.github.com>
2023-02-12 20:17:53 +0530

    created the controller for courses

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
  import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BasicsModule } from './basics/basics.module';
+import { AppService } from './app.service'; 
 import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [BasicsModule, CoursesModule],
+  imports: [CoursesModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/basics/basics.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BasicsController } from './basics.controller';
-import { BasicsService } from './basics.service';
-
-@Module({
-  controllers: [BasicsController],
-  providers: [BasicsService]
-})
-export class BasicsModule {}
