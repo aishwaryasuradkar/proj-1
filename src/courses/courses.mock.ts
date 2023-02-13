3e8b1ff9af364662aa43c07062b4beabe8fb8e74
aishwarya <98905717+aishwaryasuradkar@users.noreply.github.com>
2023-02-13 16:38:12 +0530

    successfully getcourse,add course, delete course

---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,5 @@
+export const COURSES=[
+    {id:1, title:'NodeJs- The complete guide',author:'A'},
+    {id:2, title:'The complete web developer in 2020', author:'B'},
+    {id:3, title:'Learn and understand NodeJs',author:'C'}
+];
